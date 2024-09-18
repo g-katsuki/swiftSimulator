d71fab385020db2464f4fc0a72bcfe440780e1b4
Katsuki Go <go@KatsukinoMacBook-Air.local>
2024-09-18 21:30:15 +0900

    first commit

---
+++ puyoSimulator/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  puyoSimulator
+//
+//  Created by Katsuki Go on 2024/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!!!")
+            Text("test")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ puyoSimulator/puyoSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  puyoSimulatorApp.swift
+//  puyoSimulator
+//
+//  Created by Katsuki Go on 2024/09/09.
+//
+
+import SwiftUI
+
+@main
+struct puyoSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
