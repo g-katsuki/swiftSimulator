f254d0f07513c95d504c52f9837a1bf97e61d309
Katsuki Go <go@KatsukinoMacBook-Air.local>
2024-11-30 22:50:38 +0900

    不要ファイル削除

--- puyoSimulator/puyoSimulatorApp.swift
+++ puyoSimulator/puyoSimulatorApp.swift
@@ -1,36 +1,3 @@
-//
-//  puyoSimulatorApp.swift
-//  puyoSimulator
-//
-//  Created by Katsuki Go on 2024/09/09.
-//
-
-//import SwiftUI
-//
-//@main
-//struct puyoSimulatorApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            // SwiftUIのビューを表示する場合
-//            ContentView()
-//            
-//            // UIKitのビューコントローラを表示する場合
-////             ViewControllerWrapper()
-//        }
-//    }
-//}
-//
-//// UIKitのViewControllerをSwiftUIで使用するためのラッパー
-//struct ViewControllerWrapper: UIViewControllerRepresentable {
-//    func makeUIViewController(context: Context) -> UIViewController {
-//        return ViewController() // 自作のViewControllerクラス
-//    }
-//    
-//    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-//        // 更新処理があればここに記述
-//    }
-//}
-
 
 import SwiftUI
 
